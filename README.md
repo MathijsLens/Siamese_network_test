1564155aafdb625b459a884fd4aba0e287d5c6c7
MathijsLens <mathijs.lens@gmail.com>
2022-10-20 14:25:16 +0200

    hypersearch

--- README.md
+++ README.md
@@ -8,13 +8,13 @@
 
 
 
-### BCE_LOSS:
+### Varification Model, BCE Loss:
 
 Input is pair of images, output is 1 if the images are the same number, 0 is different Numbers
 BCE is used. the embedings are concatenated and sigmoit is used to get output between 0 and 1. 
 
 
-### TRIPLET LOSS:
+### Identification Model, TRIPLET LOSS:
 
 Input is a triplet of images. One achor image, One Positve image and one Negative Image. 
 The loss is so that the distace between the anchor and the Positve embeddign is minimised, and the distace between the anchor and the negative embedding is maximised. The network creates clusters of embeddings. (same img close, differt img far away in embedded space)
--- bce_loss/main_siamese.py
+++ bce_loss/main_siamese.py
@@ -17,16 +17,10 @@ from siamese_model import SiameseNetwork
 
 def train(config, model, device, train_loader, optim, epoch, writer=None):
     model.train()
-    
     # criterion: LoSS criterion here BCE LOSS
-    
     criterion= nn.BCELoss()
-
-    
-    
-    
     # trainloop: 
-    for batch_index, (image_1, image_2, targets) in enumerate(train_loader):
+    for batch_index, (image_1, image_2, targets) in enumerate(train_loader):        
         image_1, image_2, targets=image_1.to(device), image_2.to(device), targets.to(device)
         optim.zero_grad()
         outputs  = model(image_1, image_2).squeeze()
@@ -41,10 +35,9 @@ def train(config, model, device, train_loader, optim, epoch, writer=None):
         if batch_index % config['log_interval'] ==0: 
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format( epoch, batch_index *len(image_1), len(train_loader.dataset), 
                                                                             100.0*batch_index /len(train_loader), loss.item()))
-            if writer:
-                writer.add_scalar('Training_loss', loss, global_step=batch_index)
-                writer.add_scalar('Training accuracy', running_acc, global_step=batch_index)
-        
+            
+            writer.add_scalar('Training_loss', loss, global_step=batch_index)
+            writer.add_scalar('Training accuracy', running_acc, global_step=batch_index)
 
 
        
@@ -91,21 +84,39 @@ def main():
     else:
         device = torch.device("cpu")
 
+    
     train_dataset = MNIST_BCE(config['path_dataset'], train=True, download=True)
     test_dataset =  MNIST_BCE(config['path_dataset'], train=False)
-    
-    train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=config['batch_size'], shuffle=True  )
-    test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=config['test_batch_size'], shuffle=True )
-
+    # train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=config['batch_size'], shuffle=True  )
+    # test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=config['test_batch_size'], shuffle=True )
     writer=SummaryWriter(f'../runs/bce/test')
     
     model = SiameseNetwork().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=config['learning_rate'])
-
-    scheduler = StepLR(optimizer, step_size=1, gamma=config['gamma'])
     
-    #training
-    if config['train_net']:
+    
+    # search for hyperparams: 
+    if config['hypersearch']:
+        for batch_s in config['batch_size']: 
+            for lr in config['learning_rate']:
+                writer=SummaryWriter(f'../runs/bce/batchsize{batch_s} lr {lr}')
+                train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_s, shuffle=True  )
+                test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=config['test_batch_size'], shuffle=True )
+                model = SiameseNetwork().to(device)
+                optimizer = optim.Adadelta(model.parameters(), lr=lr)
+                scheduler = StepLR(optimizer, step_size=1, gamma=config['gamma'])
+                for epoch in range(0, config['epoch']):
+                    train(config, model, device, train_loader, optimizer, epoch, writer)
+                    test(model, device, test_loader)
+                    scheduler.step()
+                    
+                
+    #train 
+    elif config['train_net']:
+        
+        optimizer = optim.Adadelta(model.parameters(), lr=config['learning_rate'])
+        scheduler = StepLR(optimizer, step_size=1, gamma=config['gamma'])
+        train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=config['batch_size'], shuffle=True  )
+        test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=config['test_batch_size'], shuffle=True )
         for epoch in range(1, config['epoch'] + 1):
             train(config, model, device, train_loader, optimizer, epoch, writer)
             test(model, device, test_loader)
@@ -115,12 +126,10 @@ def main():
             torch.save({"model_state_dict": model.state_dict(),
                     "optimzier_state_dict": optimizer.state_dict()
                 }, "../saved_models/BCE_loss.pth")
-    else: 
+    else:
+        train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=config['batch_size'], shuffle=True  )
+        test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=config['test_batch_size'], shuffle=True ) 
         test(model, device, test_loader)
-
-
-
-
 
 
 
